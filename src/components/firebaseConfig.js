e220785ccbcc7abf9e5369094097f8698104e155
adowning <ashdowning@gmail.com>
2018-06-16 15:33:36 -0500

    asdfasdf

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyD-BsWzPHYrXvuQsh0NeXF9u9T82JYzbQA",
+    authDomain: "opol6-dev.firebaseapp.com",
+    databaseURL: "https://opol6-dev.firebaseio.com",
+    projectId: "opol6-dev",
+    storageBucket: "opol6-dev.appspot.com",
+    messagingSenderId: "258468941701"
+}
